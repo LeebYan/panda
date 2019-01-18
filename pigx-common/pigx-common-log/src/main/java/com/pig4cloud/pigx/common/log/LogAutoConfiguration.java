f80d6d51e4582f10e0bc72d0b8e3db1c1b952886
冷冷 <wangiegie@gmail.com>
2019-01-18 15:48:05 +0800

    :bug: Fixing a bug. publish not import

--- pigx-common/pigx-common-log/src/main/java/com/pig4cloud/pigx/common/log/LogAutoConfiguration.java
+++ pigx-common/pigx-common-log/src/main/java/com/pig4cloud/pigx/common/log/LogAutoConfiguration.java
@@ -24,6 +24,7 @@ import com.pig4cloud.pigx.common.log.aspect.SysLogAspect;
 import com.pig4cloud.pigx.common.log.event.SysLogListener;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -47,7 +48,7 @@ public class LogAutoConfiguration {
 	}
 
 	@Bean
-	public SysLogAspect sysLogAspect() {
-		return new SysLogAspect();
+	public SysLogAspect sysLogAspect(ApplicationEventPublisher publisher) {
+		return new SysLogAspect(publisher);
 	}
 }
