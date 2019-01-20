17c23a1c8a5fc3317bd77da38e3b020be8125ee4
冷冷 <wangiegie@gmail.com>
2019-01-20 21:14:38 +0800

    :sparkles: 添加新特性，feign 自动降级

--- pigx-upms/pigx-upms-api/src/main/java/com/pig4cloud/pigx/admin/api/feign/RemoteTokenService.java
+++ pigx-upms/pigx-upms-api/src/main/java/com/pig4cloud/pigx/admin/api/feign/RemoteTokenService.java
@@ -18,7 +18,6 @@
 package com.pig4cloud.pigx.admin.api.feign;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.pig4cloud.pigx.admin.api.feign.factory.RemoteTokenServiceFallbackFactory;
 import com.pig4cloud.pigx.common.core.constant.SecurityConstants;
 import com.pig4cloud.pigx.common.core.constant.ServiceNameConstants;
 import com.pig4cloud.pigx.common.core.util.R;
@@ -31,23 +30,25 @@ import java.util.Map;
  * @author lengleng
  * @date 2018/9/4
  */
-@FeignClient(value = ServiceNameConstants.AUTH_SERVICE, fallbackFactory = RemoteTokenServiceFallbackFactory.class)
+@FeignClient(value = ServiceNameConstants.AUTH_SERVICE)
 public interface RemoteTokenService {
 	/**
 	 * 分页查询token 信息
 	 *
 	 * @param params 分页参数
+	 * @param from   内部调用标志
 	 * @return page
 	 */
 	@PostMapping("/token/page")
-	R<Page> getTokenPage(@RequestBody Map<String, Object> params,@RequestHeader(SecurityConstants.FROM) String from);
+	R<Page> getTokenPage(@RequestBody Map<String, Object> params, @RequestHeader(SecurityConstants.FROM) String from);
 
 	/**
 	 * 删除token
 	 *
 	 * @param token token
+	 * @param from  内部调用标志
 	 * @return
 	 */
 	@DeleteMapping("/token/{token}")
-	R<Boolean> removeTokenById(@PathVariable("token") String token,@RequestHeader(SecurityConstants.FROM) String from);
+	R<Boolean> removeTokenById(@PathVariable("token") String token, @RequestHeader(SecurityConstants.FROM) String from);
 }
