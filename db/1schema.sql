ab0c01da0f0b12d3fa70741cd3b12c55d13d6d0e
冷冷 <wangiegie@gmail.com>
2019-08-19 16:23:03 +0800

    :mute: 删除代码， 关于网卡配置

--- db/1schema.sql
+++ db/1schema.sql
@@ -15,3 +15,6 @@ create database `pigxx_config` default character set utf8mb4 collate utf8mb4_gen
 
 -- pigx pay配置相关库
 create database `pigxx_pay` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- pigx pay配置相关库
+create database `pigxx_codegen` default character set utf8mb4 collate utf8mb4_general_ci;
