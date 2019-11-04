e3468b5c8da929ee3ae40b8d70421383dc8f9979
冷冷 <wangiegie@gmail.com>
2019-11-04 12:54:25 +0800

    :fire: Removing code or files. 删除 hystrix 监控

--- pigx-visual/pigx-monitor/src/main/java/com/pig4cloud/pigx/monitor/PigxMonitorApplication.java
+++ pigx-visual/pigx-monitor/src/main/java/com/pig4cloud/pigx/monitor/PigxMonitorApplication.java
@@ -23,17 +23,13 @@ package com.pig4cloud.pigx.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @author lengleng
- * @date 2018年06月21日
+ * @date 2019年11月4日
  * 监控中心
  */
-@EnableTurbine
 @EnableAdminServer
-@EnableHystrixDashboard
 @SpringCloudApplication
 public class PigxMonitorApplication {
 
--- pigx-visual/pigx-monitor/src/main/resources/ui/assets/js/chunk-common.74097ff8.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-common"],{6719:function(n,o,w){}}]);
-//# sourceMappingURL=chunk-common.74097ff8.js.map
