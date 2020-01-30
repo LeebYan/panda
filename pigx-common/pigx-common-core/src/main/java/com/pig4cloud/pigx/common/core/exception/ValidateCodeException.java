65604d745a5e583cb9f34790fed16fc33bda310a
冷冷 <wangiegie@gmail.com>
2020-01-30 12:40:15 +0800

    :recycle: Refactoring code. 退出令牌重构

--- pigx-common/pigx-common-core/src/main/java/com/pig4cloud/pigx/common/core/exception/ValidateCodeException.java
+++ pigx-common/pigx-common-core/src/main/java/com/pig4cloud/pigx/common/core/exception/ValidateCodeException.java
@@ -23,7 +23,7 @@ package com.pig4cloud.pigx.common.core.exception;
  * @author lengleng
  * @date 2018年06月22日16:22:15
  */
-public class ValidateCodeException extends Exception {
+public class ValidateCodeException extends RuntimeException {
 	private static final long serialVersionUID = -7285211528095468156L;
 
 	public ValidateCodeException() {
