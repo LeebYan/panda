d1b2fc2e2b3dfb7ae935566811d341538d17ec2e
lengleng <wangiegie@gmail.com>
2020-06-06 14:26:38 +0800

    :recycle: Refactoring code.  优化 Jackconfig 避免spring boot admin json 输出异常

--- pigx-visual/pigx-monitor/src/main/java/com/pig4cloud/pigx/monitor/config/JacksonConfig.java
+++ pigx-visual/pigx-monitor/src/main/java/com/pig4cloud/pigx/monitor/config/JacksonConfig.java
@@ -1,13 +1,13 @@
-package com.pig4cloud.pigx.common.core.config;
+package com.pig4cloud.pigx.monitor.config;
 
 import cn.hutool.core.date.DatePattern;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.pig4cloud.pigx.common.core.jackson.PigxJavaTimeModule;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import de.codecentric.boot.admin.server.config.AdminServerWebConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,17 +24,22 @@ import java.util.TimeZone;
  */
 @Configuration
 @ConditionalOnClass(ObjectMapper.class)
-@AutoConfigureBefore(JacksonAutoConfiguration.class)
+@AutoConfigureAfter(AdminServerWebConfiguration.class)
 public class JacksonConfig {
 
+	/**
+	 * 覆盖core 模块提供的序列方式，增加springboot admin 序列化
+	 *
+	 * @param adminJacksonModule spring boot admin 提供的序列化
+	 * @return
+	 */
 	@Bean
-	@ConditionalOnMissingBean
-	public Jackson2ObjectMapperBuilderCustomizer customizer() {
+	public Jackson2ObjectMapperBuilderCustomizer customizer(SimpleModule adminJacksonModule) {
 		return builder -> {
 			builder.locale(Locale.CHINA);
 			builder.timeZone(TimeZone.getTimeZone("Asia/Shanghai"));
 			builder.simpleDateFormat(DatePattern.NORM_DATETIME_PATTERN);
-			builder.modules(new PigxJavaTimeModule());
+			builder.modules(new PigxJavaTimeModule(), adminJacksonModule);
 		};
 	}
 }
