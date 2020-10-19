25c3f937cc313b49584eef882c1ca213b0961e5e
pig666 <pig666@qq.com>
2020-10-19 22:29:42 +0800

    :sparkles: Introducing new features. #1944 支持分布式应用 druid sql 监控

---
+++ pigx-visual/pigx-monitor/src/main/java/com/pig4cloud/pigx/monitor/controller/DruidController.java
@@ -0,0 +1,10 @@
+package com.pig4cloud.pigx.monitor.controller;
+
+/**
+ * @author lengleng
+ * @date 2020/10/19
+ * <p>
+ * druid 监控控制器
+ */
+public class DruidController {
+}
---
+++ pigx-visual/pigx-monitor/src/main/java/com/pig4cloud/pigx/monitor/support/MonitorViewServlet.java
@@ -0,0 +1,35 @@
+package com.pig4cloud.pigx.monitor.support;
+
+import com.alibaba.druid.support.http.ResourceServlet;
+import com.pig4cloud.pigx.common.core.util.SpringContextHolder;
+import com.pig4cloud.pigx.monitor.service.impl.MonitorStatService;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletException;
+
+/**
+ * @author linchtech
+ * @date 2020-09-16 11:10
+ **/
+@Slf4j
+public class MonitorViewServlet extends ResourceServlet {
+
+    private MonitorStatService monitorStatService;
+
+    public MonitorViewServlet() {
+        super("druid");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        log.info("init MonitorViewServlet");
+        super.init();
+        monitorStatService = SpringContextHolder.getBean(MonitorStatService.class);
+    }
+
+    @Override
+    protected String process(String url) {
+        return monitorStatService.service(url);
+    }
+
+}
