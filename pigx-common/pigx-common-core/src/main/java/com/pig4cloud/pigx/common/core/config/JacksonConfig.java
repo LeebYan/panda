a648988bbf29c80d2a30943cbfc42d09cd446916
lengleng <wangiegie@gmail.com>
2020-12-07 10:48:49 +0800

    :sparkles: Introducing new features. #2114 增加全局编码转换，避免 context type 不规范乱码问题

--- pigx-common/pigx-common-core/src/main/java/com/pig4cloud/pigx/common/core/config/JacksonConfig.java
+++ pigx-common/pigx-common-core/src/main/java/com/pig4cloud/pigx/common/core/config/JacksonConfig.java
@@ -10,12 +10,15 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.web.servlet.filter.OrderedCharacterEncodingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.charset.StandardCharsets;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -65,6 +68,19 @@ public class JacksonConfig implements WebMvcConfigurer {
 		registrar.setDateFormatter(DateTimeFormatter.ofPattern(DatePattern.NORM_DATE_PATTERN));
 		registrar.setDateTimeFormatter(DateTimeFormatter.ofPattern(DatePattern.NORM_DATETIME_PATTERN));
 		registrar.registerFormatters(registry);
+	}
+
+	/**
+	 * 避免form 提交 context-type 不规范中文乱码
+	 * @return Filter
+	 */
+	@Bean
+	public OrderedCharacterEncodingFilter characterEncodingFilter() {
+		OrderedCharacterEncodingFilter filter = new OrderedCharacterEncodingFilter();
+		filter.setEncoding(StandardCharsets.UTF_8.name());
+		filter.setForceEncoding(true);
+		filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return filter;
 	}
 
 }
